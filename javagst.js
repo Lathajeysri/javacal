2b77af54e8567a4e306a6486769f5094efb034bb
Lathajeysri <lajeysri@gmail.com>
2022-11-18 04:49:36 +0530

    updated

--- javagst.js
+++ javagst.js
@@ -1,25 +1,25 @@
 
 function gstCalc(){
  
-   var pp=parseInt(document.getElementById("pp").value);
+   var productPrice=parseInt(document.getElementById("productPrice").value);
    var pr=document.getElementById("rate").value;
-var ps=document.getElementById("ps").value;
+var productState=document.getElementById("productState").value;
 
-if(ps==0){
+if(productState==0){
 var sgstPer=pr/2;
 var cgstPer=pr/2;
-var cgstAmt=pp*cgstPer/100;
-var sgstAmt=pp*sgstPer/100;
-var pay=pp+(pp*sgstPer/100)+(pp*cgstPer/100);
-document.getElementById("totalAmt").innerHTML="Rs"+pay;
+var cgstAmt=productPrice*cgstPer/100;
+var sgstAmt=productPrice*sgstPer/100;
+var pay=productPrice+cgstAmt+sgstAmt;
+document.getElementById("totalAmt").innerHTML="Rs"+Math.round(pay);
 document.getElementById("sgst").innerHTML="Rs"+sgstAmt;
 document.getElementById("cgst").innerHTML="Rs"+cgstAmt;
 
 }
 else{
    var sgstPer=pr;
-   var sgstAmt=pp*sgstPer/100;
-var pay=pp+(pp*sgstPer/100);
+   var sgstAmt=productPrice*sgstPer/100;
+var pay=productPrice+(productPrice*sgstPer/100);
 document.getElementById("totalAmt").innerHTML="Rs"+pay;
 document.getElementById("sgst").innerHTML="Rs"+sgstAmt;
 document.getElementById("cgst").innerHTML=0     ;
